0b3e6b4ccaa99992e12e6b1d136f85681612e46d
Bari077 <skyline.agency@yandex.ru>
2022-11-17 08:49:30 +0300

    feat: перенести в Popup.js всю логику открытия и закрытия модалки

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,10 +1,13 @@
-class Popup {
+import { closeButtons } from "../pages/index.js"
+
+export default class Popup {
     constructor(selector) {
         this._modal = document.querySelector(selector);
     }
 
     open() {
         this._modal.classList.add('popup_opened');
+        this.setEventListeners();
     }
 
     close() {
@@ -12,10 +15,29 @@ class Popup {
     }
 
     _handleEscClose() {
-        if (evt.key === "Escape") {
+        if (event.key === "Escape") {
             this.close();
         }
     }
 
+    _handleOverlayClose() {
+        if(event.target === event.currentTarget) {
+            this.close();
+          } 
+    }
+
+    setEventListeners() {
+        window.addEventListener('keydown', () => {
+            this._handleEscClose();
+        });
+        this._modal.addEventListener('mousedown', () => {
+            this._handleOverlayClose();
+        });        
+        closeButtons.forEach(el => {
+            el.addEventListener('click', () => {
+                this.close();
+            })
+        });
+    }
    
 }
